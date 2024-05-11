421292fc4a96af9987b3a95569a116b330fa4927
Felipe Carvalho de Souza <desouzafelipecarvalho@gmail.com>
2024-05-11 16:33:22 -0300

    Upgrade versão do MySql

--- db.tf
+++ db.tf
@@ -16,33 +16,37 @@ resource "aws_db_instance" "goodburguer" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "mysql"
-  engine_version       = "5.7"
+  engine_version       = "8.0"
   instance_class       = "db.t3.micro"
   username             = var.DATABASE_USER
   password             = var.DATABASE_PASSWORD
-  parameter_group_name = "default.mysql5.7"
+  parameter_group_name = "default.mysql8.0"
 
   publicly_accessible  = true 
 
   # Adiciona o grupo de segurança
-  vpc_security_group_ids = [aws_security_group.goodburguerdb_security_group.id]
+  vpc_security_group_ids = [data.aws_security_group.goodburguerdb_security_group.id]
 }
 
-resource "aws_security_group" "goodburguerdb_security_group" {
-  name        = "goodburguerdb_security_group"
-  description = "Permite trafego de entrada para o banco de dados RDS"
+#resource "aws_security_group" "goodburguerdb_security_group" {
+#  name        = "goodburguerdb_security_group"
+#  description = "Permite trafego de entrada para o banco de dados RDS"
+#
+#  ingress {
+#    from_port   = 3306
+#    to_port     = 3306
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]  #
+#  }
+#
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#}
 
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  #
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+data "aws_security_group" "goodburguerdb_security_group" {
+  name = "goodburguerdb_security_group"
 }
