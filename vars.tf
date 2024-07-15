03289192a73bf16c5f43b5bd8b25dca4d5ac45fa
Felipe Carvalho de Souza <desouzafelipecarvalho@gmail.com>
2024-07-15 09:07:15 -0300

    Ajustando projeto terraform do mysql

--- vars.tf
+++ vars.tf
@@ -6,6 +6,18 @@ variable "projectName" {
   default = "goodburguer"
 }
 
+variable "subnetA" {
+  default = "subnet-0c1d5d6102025a707"
+}
+
+variable "subnetB" {
+  default = "subnet-00b9d57e8a157ac97"
+}
+
+variable "subnetC" {
+  default = "subnet-0183c3c66a25b44ed"
+}
+
 variable "AWS_ACCESS_KEY_ID" {
 
 }
